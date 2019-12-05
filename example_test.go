2ecf190d30207851053f2f29532f77352acc7cc7
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-12-05 17:50:30 +0300

    improved examples

--- example_test.go
+++ example_test.go
@@ -20,6 +20,9 @@ func Example() {
 	xlog.V(1).Warning("this is warning log, verbosity 1.")
 	xlog.V(2).Error("this is error log, verbosity 2.")
 	xlog.V(3).Error("this is error log, verbosity 3. it will not be shown.")
+
+	xlog.WithFields(xlog.Field{Key: "a", Val: "11"}).Info("this is info log, verbosity 0 with fields.")
+	xlog.WithFieldKeyVals("x", "1", "y", "2").Info("this is info log, verbosity 0 with fields.")
 }
 
 func ExampleLogger() {
