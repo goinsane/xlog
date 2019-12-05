766abe9603dd4dc49bd0d504ef4f61d9102da473
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-12-05 16:59:25 +0300

    improved examples

--- example_test.go
+++ example_test.go
@@ -1,12 +1,64 @@
 package xlog_test
 
 import (
+	"os"
 	"time"
 
 	"github.com/goinsane/xlog"
 )
 
 func Example() {
+	xlog.Info("this is info log, verbosity 0.")
+	xlog.Debug("this is debug log, verbosity 0. it will not be shown.")
+
+	xlog.SetSeverity(xlog.SeverityDebug)
+	xlog.Debug("this is debug log, verbosity 0.")
+	xlog.V(0).Warning("this is warning log, verbosity 0.")
+	xlog.V(1).Warning("this is warning log, verbosity 1. it will not be shown.")
+
+	xlog.SetVerbose(2)
+	xlog.V(1).Warning("this is warning log, verbosity 1.")
+	xlog.V(2).Error("this is error log, verbosity 2.")
+	xlog.V(3).Error("this is error log, verbosity 3. it will not be shown.")
+}
+
+func ExampleLogger() {
+	logger := xlog.New(xlog.NewTextOutput(os.Stdout, xlog.OutputFlagSeverity), xlog.SeverityInfo, 2)
+
+	logger.Info("this is info log, verbosity 0.")
+	logger.V(0).Warning("this is warning log, verbosity 0.")
+	logger.V(1).Warning("this is warning log, verbosity 1.")
+	logger.V(2).Error("this is error log, verbosity 2.")
+	logger.V(3).Error("this is error log, verbosity 3. it will not be shown.")
+	logger.Debug("this is debug log, verbosity 0. it will not be shown.")
+
+	// Output:
+	// INFO: this is info log, verbosity 0.
+	// WARNING: this is warning log, verbosity 0.
+	// WARNING: this is warning log, verbosity 1.
+	// ERROR: this is error log, verbosity 2.
+}
+
+func ExampleTextOutput() {
+	output := xlog.NewTextOutput(os.Stdout, xlog.OutputFlagSeverity)
+	logger := xlog.New(output, xlog.SeverityInfo, 2)
+
+	logger.Info("this is info log, verbosity 0.")
+	logger.V(0).Warning("this is warning log, verbosity 0.")
+	logger.V(1).Warning("this is warning log, verbosity 1.")
+	logger.V(2).Error("this is error log, verbosity 2.")
+	logger.V(3).Error("this is error log, verbosity 3. it will not be shown.")
+	logger.Debug("this is debug log, verbosity 0. it will not be shown.")
+
+	// Output:
+	// INFO: this is info log, verbosity 0.
+	// WARNING: this is warning log, verbosity 0.
+	// WARNING: this is warning log, verbosity 1.
+	// ERROR: this is error log, verbosity 2.
+}
+
+func ExamplePkg() {
+	// needing to reset previous example changes
 	xlog.Reset()
 
 	// set output flags for testable results
