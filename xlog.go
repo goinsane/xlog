973140a2bd7b97f2964809cbffd9ae1d3fcedc09
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-06-21 14:32:18 +0300

    improved godoc in xlog.go. renamed some variables to defaultLogger and defaultOutput. added defaultPCSize variable. improved Reset() function.

--- xlog.go
+++ xlog.go
@@ -5,198 +5,204 @@ import (
 	"io"
 	"os"
 	"time"
+
+	"github.com/goinsane/erf"
 )
 
 var (
-	defLogger *Logger     = New(defOutput, SeverityInfo, 0)
-	defOutput *TextOutput = NewTextOutput(os.Stderr, FlagDefault)
+	defaultLogger *Logger     = New(defaultOutput, SeverityInfo, 0)
+	defaultOutput *TextOutput = NewTextOutput(os.Stderr)
+
+	defaultPCSize = erf.DefaultPCSize
 )
 
-// DefaultLogger returns the default logger.
+// DefaultLogger returns the default Logger.
 func DefaultLogger() *Logger {
-	return defLogger
+	return defaultLogger
 }
 
-// DefaultOutput returns the default output as Output. Type of default output is TextOutput.
+// DefaultOutput returns the default Output as Output. Type of the default Output is TextOutput.
 func DefaultOutput() Output {
-	return defOutput
+	return defaultOutput
 }
 
-// Fatal logs to the FATAL severity logs to the default logger, then calls os.Exit(1).
+// Fatal logs to the FATAL severity logs to the default Logger, then calls os.Exit(1).
 func Fatal(args ...interface{}) {
-	defLogger.log(SeverityFatal, args...)
+	defaultLogger.log(SeverityFatal, args...)
 	os.Exit(1)
 }
 
-// Fatalf logs to the FATAL severity logs to the default logger, then calls os.Exit(1).
+// Fatalf logs to the FATAL severity logs to the default Logger, then calls os.Exit(1).
 func Fatalf(format string, args ...interface{}) {
-	defLogger.logf(SeverityFatal, format, args...)
+	defaultLogger.logf(SeverityFatal, format, args...)
 	os.Exit(1)
 }
 
-// Fatalln logs to the FATAL severity logs to the default logger, then calls os.Exit(1).
+// Fatalln logs to the FATAL severity logs to the default Logger, then calls os.Exit(1).
 func Fatalln(args ...interface{}) {
-	defLogger.logln(SeverityFatal, args...)
+	defaultLogger.logln(SeverityFatal, args...)
 	os.Exit(1)
 }
 
-// Error logs to the ERROR severity logs to the default logger.
+// Error logs to the ERROR severity logs to the default Logger.
 func Error(args ...interface{}) {
-	defLogger.log(SeverityError, args...)
+	defaultLogger.log(SeverityError, args...)
 }
 
-// Errorf logs to the ERROR severity logs to the default logger.
+// Errorf logs to the ERROR severity logs to the default Logger.
 func Errorf(format string, args ...interface{}) {
-	defLogger.logf(SeverityError, format, args...)
+	defaultLogger.logf(SeverityError, format, args...)
 }
 
-// Errorln logs to the ERROR severity logs to the default logger.
+// Errorln logs to the ERROR severity logs to the default Logger.
 func Errorln(args ...interface{}) {
-	defLogger.logln(SeverityError, args...)
+	defaultLogger.logln(SeverityError, args...)
 }
 
-// Warning logs to the WARNING severity logs to the default logger.
+// Warning logs to the WARNING severity logs to the default Logger.
 func Warning(args ...interface{}) {
-	defLogger.log(SeverityWarning, args...)
+	defaultLogger.log(SeverityWarning, args...)
 }
 
-// Warningf logs to the WARNING severity logs to the default logger.
+// Warningf logs to the WARNING severity logs to the default Logger.
 func Warningf(format string, args ...interface{}) {
-	defLogger.logf(SeverityWarning, format, args...)
+	defaultLogger.logf(SeverityWarning, format, args...)
 }
 
-// Warningln logs to the WARNING severity logs to the default logger.
+// Warningln logs to the WARNING severity logs to the default Logger.
 func Warningln(args ...interface{}) {
-	defLogger.logln(SeverityWarning, args...)
+	defaultLogger.logln(SeverityWarning, args...)
 }
 
-// Info logs to the INFO severity logs to the default logger.
+// Info logs to the INFO severity logs to the default Logger.
 func Info(args ...interface{}) {
-	defLogger.log(SeverityInfo, args...)
+	defaultLogger.log(SeverityInfo, args...)
 }
 
-// Infof logs to the INFO severity logs to the default logger.
+// Infof logs to the INFO severity logs to the default Logger.
 func Infof(format string, args ...interface{}) {
-	defLogger.logf(SeverityInfo, format, args...)
+	defaultLogger.logf(SeverityInfo, format, args...)
 }
 
-// Infoln logs to the INFO severity logs to the default logger.
+// Infoln logs to the INFO severity logs to the default Logger.
 func Infoln(args ...interface{}) {
-	defLogger.logln(SeverityInfo, args...)
+	defaultLogger.logln(SeverityInfo, args...)
 }
 
-// Debug logs to the DEBUG severity logs to the default logger.
+// Debug logs to the DEBUG severity logs to the default Logger.
 func Debug(args ...interface{}) {
-	defLogger.log(SeverityDebug, args...)
+	defaultLogger.log(SeverityDebug, args...)
 }
 
-// Debugf logs to the DEBUG severity logs to the default logger.
+// Debugf logs to the DEBUG severity logs to the default Logger.
 func Debugf(format string, args ...interface{}) {
-	defLogger.logf(SeverityDebug, format, args...)
+	defaultLogger.logf(SeverityDebug, format, args...)
 }
 
-// Debugln logs to the DEBUG severity logs to the default logger.
+// Debugln logs to the DEBUG severity logs to the default Logger.
 func Debugln(args ...interface{}) {
-	defLogger.logln(SeverityDebug, args...)
+	defaultLogger.logln(SeverityDebug, args...)
 }
 
-// Print logs to the default logger.
+// Print logs to the default Logger.
 func Print(args ...interface{}) {
-	defLogger.log(defLogger.printSeverity, args...)
+	defaultLogger.log(defaultLogger.printSeverity, args...)
 }
 
-// Printf logs to the default logger.
+// Printf logs to the default Logger.
 func Printf(format string, args ...interface{}) {
-	defLogger.logf(defLogger.printSeverity, format, args...)
+	defaultLogger.logf(defaultLogger.printSeverity, format, args...)
 }
 
-// Println logs to the default logger.
+// Println logs to the default Logger.
 func Println(args ...interface{}) {
-	defLogger.logln(defLogger.printSeverity, args...)
+	defaultLogger.logln(defaultLogger.printSeverity, args...)
 }
 
-// SetOutput sets the default logger's output. By default, the default output.
+// SetOutput sets the default Logger's output. By default, the default Output.
 func SetOutput(output Output) {
-	defLogger.SetOutput(output)
+	defaultLogger.SetOutput(output)
 }
 
-// SetSeverity sets the default logger's severity. If severity is invalid, it sets SeverityInfo.
+// SetSeverity sets the default Logger's severity. If severity is invalid, it sets SeverityInfo.
 // By default, SeverityInfo.
 func SetSeverity(severity Severity) {
-	defLogger.SetSeverity(severity)
+	defaultLogger.SetSeverity(severity)
 }
 
-// SetVerbose sets the default logger's verbose.
+// SetVerbose sets the default Logger's verbose.
 // By default, 0.
 func SetVerbose(verbose Verbose) {
-	defLogger.SetVerbose(verbose)
+	defaultLogger.SetVerbose(verbose)
 }
 
-// SetFlags sets the default logger's flags.
+// SetFlags sets the default Logger's flags.
 // By default, FlagDefault.
 func SetFlags(flags Flag) {
-	defLogger.SetFlags(flags)
+	defaultLogger.SetFlags(flags)
 }
 
-// SetPrintSeverity sets the default logger's severity level which is using with Print functions.
+// SetPrintSeverity sets the default Logger's severity level which is using with Print methods.
 // If printSeverity is invalid, it sets SeverityInfo. By default, SeverityInfo.
 func SetPrintSeverity(printSeverity Severity) {
-	defLogger.SetPrintSeverity(printSeverity)
+	defaultLogger.SetPrintSeverity(printSeverity)
 }
 
-// SetStackTraceSeverity sets the default logger's severity level which allows printing stack trace.
+// SetStackTraceSeverity sets the default Logger's severity level which allows printing stack trace.
 // If stackTraceSeverity is invalid, it sets SeverityNone. By default, SeverityNone.
 func SetStackTraceSeverity(stackTraceSeverity Severity) {
-	defLogger.SetStackTraceSeverity(stackTraceSeverity)
+	defaultLogger.SetStackTraceSeverity(stackTraceSeverity)
 }
 
-// V clones the default logger if Logger's verbose is greater or equal than given verbosity. Otherwise returns nil.
+// V clones the default Logger if Logger's verbose is greater or equal than given verbosity. Otherwise returns nil.
 func V(verbosity Verbose) *Logger {
-	return defLogger.V(verbosity)
+	return defaultLogger.V(verbosity)
 }
 
 // WithPrefix clones the default Logger and adds given prefix to end of the underlying prefix.
 func WithPrefix(args ...interface{}) *Logger {
-	return defLogger.WithPrefix(args...)
+	return defaultLogger.WithPrefix(args...)
 }
 
 // WithPrefixf clones the default Logger and adds given prefix to end of the underlying prefix.
 func WithPrefixf(format string, args ...interface{}) *Logger {
-	return defLogger.WithPrefixf(format, args...)
+	return defaultLogger.WithPrefixf(format, args...)
 }
 
-// WithTime clones the default logger with given time.
+// WithTime clones the default Logger with given time.
 func WithTime(tm time.Time) *Logger {
-	return defLogger.WithTime(tm)
+	return defaultLogger.WithTime(tm)
 }
 
-// WithFields clones the default logger with given fields.
+// WithFields clones the default Logger with given fields.
 func WithFields(fields ...Field) *Logger {
-	return defLogger.WithFields(fields...)
+	return defaultLogger.WithFields(fields...)
 }
 
-// WithFieldKeyVals clones default logger with given key and values of Field.
+// WithFieldKeyVals clones the default Logger with given key and values of Field.
 func WithFieldKeyVals(kvs ...interface{}) *Logger {
-	return defLogger.WithFieldKeyVals(kvs...)
+	return defaultLogger.WithFieldKeyVals(kvs...)
 }
 
-// SetOutputWriter sets the default output writer.
+// SetOutputWriter sets the default Output's writer.
 func SetOutputWriter(w io.Writer) {
-	defOutput.SetWriter(w)
+	defaultOutput.SetWriter(w)
 }
 
-// SetOutputFlags sets the default output flags.
+// SetOutputFlags sets the default Output's flags to override every single Log.Flags if the flags argument different than 0.
+// By default, 0.
 func SetOutputFlags(flags Flag) {
-	defOutput.SetFlags(flags)
+	defaultOutput.SetFlags(flags)
 }
 
-// Reset resets default logger and output options.
+// Reset resets the default Logger and the default Output.
 func Reset() {
-	SetOutput(defOutput)
+	SetOutput(defaultOutput)
 	SetSeverity(SeverityInfo)
 	SetVerbose(0)
+	SetFlags(FlagDefault)
 	SetPrintSeverity(SeverityInfo)
 	SetStackTraceSeverity(SeverityNone)
 	SetOutputWriter(os.Stderr)
-	SetOutputFlags(FlagDefault)
+	SetOutputFlags(0)
 }
