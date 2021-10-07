c00e0bd60870932c187b84d5189e82cb73b97866
Orkun Karaduman <orkunkaraduman@gmail.com>
2021-10-07 16:05:36 +0300

    updated examples/example1.go

--- examples/example1.go
+++ examples/example1.go
@@ -1,3 +1,4 @@
+//go:build examples
 // +build examples
 
 package main
@@ -9,57 +10,60 @@ import (
 	"github.com/goinsane/xlog"
 )
 
+var (
+	testTime, _ = time.ParseInLocation("2006-01-02T15:04:05", "2010-11-12T13:14:15", time.Local)
+)
+
 func main() {
-	// reset xlog for previous changes
+	// reset xlog for previous changes if it is running in go test.
 	xlog.Reset()
-	xlog.SetFlags(xlog.FlagSeverity)
+	// change writer of default output to stdout from stderr.
 	xlog.SetOutputWriter(os.Stdout)
 
-	xlog.Debug("this is debug log, verbosity 0. it will not be shown.")
-	xlog.Info("this is info log, verbosity 0.")
-	xlog.Warning("this is warning log, verbosity 0.")
-	xlog.Error("this is error log, verbosity 0.")
-	xlog.Print("this is info log, verbosity 0 caused by Print().")
-	xlog.V(1).Info("this is info log, verbosity 1. it will not be shown.")
+	// log by Severity.
+	// default severity is SeverityInfo.
+	// default verbose is 0.
+	xlog.Debug("this is debug log. but it won't be shown.")
+	xlog.Info("this is info log.")
+	xlog.Warning("this is warning log.")
+	xlog.V(1).Error("this is error log, verbosity 1. but it won't be shown.")
 
+	// SetSeverity()
 	xlog.SetSeverity(xlog.SeverityDebug)
-	xlog.Debug("this is debug log, verbosity 0.")
+	xlog.Debug("this is debug log. it will now be shown.")
 
+	// SetVerbose() and V()
 	xlog.SetVerbose(1)
-	xlog.V(0).Info("this is info log, verbosity 0.")
-	xlog.V(1).Info("this is info log, verbosity 1.")
-	xlog.V(2).Info("this is info log, verbosity 2. it will not be shown.")
+	xlog.V(1).Error("this is error log, verbosity 1. it will now be shown.")
+	xlog.V(2).Warning("this is warning log, verbosity 2. it won't be shown.")
 
+	// SetFlags()
+	// default flags is FlagDefault.
+	xlog.SetFlags(xlog.FlagDefault | xlog.FlagShortFile)
+	xlog.Info("this is info log. you can see file name and line in this log.")
+
+	// log using Print.
+	// default print severity is SeverityInfo.
+	xlog.Print("this log will be shown as info log.")
+
+	// SetPrintSeverity()
 	xlog.SetPrintSeverity(xlog.SeverityWarning)
-	xlog.Print("this is warning log, verbosity 0 caused by Print().")
+	xlog.Print("this log will now be shown as warning log.")
 
-	xlog.Warning("this is warning log, verbosity 0.\nwithout padding.")
-	xlog.SetFlags(xlog.FlagSeverity | xlog.FlagPadding)
-	xlog.Warning("this is warning log, verbosity 0.\nwith padding.")
+	// SetStackTraceSeverity()
+	// default stack trace severity is none.
+	xlog.SetStackTraceSeverity(xlog.SeverityWarning)
+	xlog.Warning("this is warning log. you can see stack trace end of this log.")
+	xlog.Error("this is error log. you can still see stack trace end of this log.")
+	xlog.Info("this is info log. stack trace won't be shown end of this log.")
 
-	xlog.SetFlags(xlog.FlagSeverity | xlog.FlagPadding | xlog.FlagFields)
-	xlog.WithFieldKeyVals("key1", "val1", "key2", "val2", "key3", "val3", "key1", "val1-2", "key2", "val2-2").Error("this is error log, verbosity 0.\nwith padding.\nwith fields.")
+	// WithPrefix()
+	xlog.WithPrefix("prefix1").Warning("this is warning log with prefix 'prefix1'.")
+	xlog.WithPrefix("prefix1").WithPrefix("prefix2").Error("this is error log with both of prefixes 'prefix1' and 'prefix2'.")
 
-	xlog.SetFlags(xlog.FlagDefault)
-	tm, _ := time.ParseInLocation("2006-01-02T15:04:05", "2019-11-13T21:56:24", time.Local)
-	xlog.WithTime(tm).Info("this is info log, verbosity 0.")
+	// WithTime()
+	xlog.WithTime(testTime).Info("this is info log with custom time.")
 
-	// Output:
-	// INFO - this is info log, verbosity 0.
-	// WARNING - this is warning log, verbosity 0.
-	// ERROR - this is error log, verbosity 0.
-	// INFO - this is info log, verbosity 0 caused by Print().
-	// DEBUG - this is debug log, verbosity 0.
-	// INFO - this is info log, verbosity 0.
-	// INFO - this is info log, verbosity 1.
-	// WARNING - this is warning log, verbosity 0 caused by Print().
-	// WARNING - this is warning log, verbosity 0.
-	// without padding.
-	// WARNING - this is warning log, verbosity 0.
-	//           with padding.
-	// ERROR - this is error log, verbosity 0.
-	//         with padding.
-	//         with fields.
-	// 	key1="val1" key2="val2" key3="val3" key1="val1-2" key2="val2-2"
-	// 2019/11/13 21:56:24 INFO - this is info log, verbosity 0.
+	// WithFieldKeyVals()
+	xlog.WithFieldKeyVals("key1", "val1", "key2", "val2", "key3", "val3", "key1", "val1-2", "key2", "val2-2").Info("this is info log with several fields.")
 }
