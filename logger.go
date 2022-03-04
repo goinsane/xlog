51e77fac5c171c7d976caa67839eca3302833be4
Orkun Karaduman <orkunkaraduman@gmail.com>
2022-03-04 14:30:53 +0300

    use erf error with zero program counters if erfStackTrace true, in logger.out()

--- logger.go
+++ logger.go
@@ -93,12 +93,13 @@ func (l *Logger) out(severity Severity, message string, err error) {
 			if l.stackTraceSeverity >= severity {
 				log.StackTrace = e.StackTrace()
 			}
-			e2 := e.Unwrap()
+			/*e2 := e.Unwrap()
 			if _, ok := e2.(*erf.Erf); ok {
 				log.Error = e2
 			} else {
 				log.Error = e.CopyByTop(e.PCLen())
-			}
+			}*/
+			log.Error = e.CopyByTop(e.PCLen())
 		} else {
 			log.StackCaller = erf.NewStackTrace(erf.PC(1, 5)...).Caller(0)
 			if l.stackTraceSeverity >= severity {
